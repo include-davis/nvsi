284fb4f87fbb2835e41fd171147fd6c4517315f3
Derek Ma <danderekma@gmail.com>
2022-02-05 10:29:22 -0800

    Add welcome-header and welcome-body

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div id="home-page">
+      <h1 id="welcome-header">WELCOME</h1>
+      <body id="welcome-body">
+        Welcome to the New Viet Nam Studies Initiative where we bring together
+        scholars, industry experts, government officials, scientists, activists,
+        and artists to lend their knowledge of the many contemporary dimensions
+        of Viet Nam and its diasporic population. Highlighting the economic
+        reconstruction and cultural renewal through programing and publication
+        directly engages with the challenges and successes of this dynamic
+        nation.
+      </body>
+    </div>
+  );
+};
+
+export default Home;
