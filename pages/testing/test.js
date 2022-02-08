0266a239cb0bb553abd226e34210e77ed2c9d4b0
Khoi Nguyen <minhkhoi632000@gmail.com>
2022-02-08 02:51:55 -0800

    Tried creating the circle for NVSI Address component. Too hard!
    I feel like I am going crazy

--- pages/testing/test.js
+++ pages/testing/test.js
@@ -1,4 +1,7 @@
 import Address from "../../components/address";
+import Circle from "../../components/addressCircle";
+
+
 // New Viet Nam Studies Initiative
 // 3113 Hart Hall
 // University of California Davis
@@ -22,13 +25,87 @@ const contact = [
     "Phone: (530) 333 3137"
 ]
 
+import styles from '../../libs/curvedText.module.css'
+
+function UpdateCurvedText ({ curvedText, radius }) {
+    curvedText.props.style.minWidth = "initial";
+    curvedText.props.style.minHeight = "initial";
+
+    let w = curvedText.props.style.width, h = curvedText.props.style.height;
+
+
+    curvedText.props.style.minWidth = w;
+    curvedText.props.style.minHeight = h;
+
+    w = w.slice(0, -2)
+    w = parseInt(w)
+    h = h.slice(0, -2)
+    h = parseInt(h)
+
+    let spans = [];
+    let text = curvedText.props.children;
+
+
+    let circleLength = 2 * Math.PI * radius;
+
+    let angleRad = w / (2 * radius);
+
+    let angle = 2 * angleRad * 180 / Math.PI / text.length;
+
+    Array.from(text).forEach((letter, index) => {
+
+        spans.push(<span key={index} className={styles.spanItem} style={{
+            position: "absolute",
+            height: `${radius}px`,
+            transformOrigin: "bottom center",
+        }}>
+            {letter}
+        </span>);
+    });
+
+    Array.from(spans).forEach(( elem, index ) => {
+        elem.props.style.transform = `translate(${w / 2}px,0px) rotate(${index * angle - text.length * angle / 2}deg)`
+        console.log(elem.props.style)
+    })
+
+    return (
+        <div className={styles.text}>
+            {spans}
+        </div>
+    )
+}
+
+
 export default function Test () {
+    // const text = "New Viet Nam Studies Initiative";
+    // const [textStyle, changeTextSyle] = useMeasure(text);
+    console.log("Here")
+    console.log(styles.text)
+    const text = <p
+        style={{
+            minWidth: '0.5em',
+            minHeight: '0.5em',
+            width: '500px',
+            height: '500px',
+            
+            position: 'relative',
+            display: 'inline-block',
+            margin: '0 auto',
+            fontSize: '100px'
+        }}
+        >Wow this is awesome
+    </p>;
+    console.log(text);
     return (
         <>
-            <Address PhysAddress={address} ContactInfo={contact} />
-
+            <UpdateCurvedText curvedText={text} radius={1000} />
+            <Circle Text={"New Viet Nam Studies Initiative"} Type="left" />
+            {/* <Address PhysAddress={address} ContactInfo={contact} /> */}
             {/* <p className={styles.rotateLeft}>{PhysAddress[0]}</p>
             <p className={styles.rotateRight}>{PhysAddress[0]}</p> */}
         </>
     );
 }
+
+
+
