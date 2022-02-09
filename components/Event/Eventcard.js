d1446b73b17f5074a061ae164f56703c1140c4da
s3lven <erizjsub2@gmail.com>
2022-02-09 15:35:17 -0800

    Eventcard changes, moved fbk to public

--- components/Event/Eventcard.js
+++ components/Event/Eventcard.js
@@ -1,18 +1,18 @@
-/* eslint-disable @next/next/no-img-element */
-import "../../styles/Event/Eventcard.module.css"
+import styles from './Eventcard.module.css'
 import Image from 'next/image'
 
 export default function Eventcard() {
     return (
-        <div className='event1-container'>
-            <div className='image-container'>
-                <img src="fubuki.jpg" alt="Fubuki"/>
+        <div className={styles.event}>
+            <div className={styles.image}>
+                <Image src="/fubuki.jpg" alt="Fubuki" width="500" height="500" layout='intrinsic'/>
             </div>
-            <div className='desc-container'>
+            <div className={styles.desc}>
+                {/* TODO: Fill in data with props*/}
                 <h3>Viet Nam and the Diaspora Silicon Valley Connections</h3>
                 <b>Feb 23, 2018 @ 12:00 - 1:00pm</b>
                 <p>Vietnamese and Vietnamese Americans have an important part of the Silicon Valley since its early development. It began with the thousands of Vietnamese refugees and immigrant assembly workers in companies like IBM and Hewlett Packard.</p>
-                <button>Event Details {">"}</button>
+                <button id="eventbutton">Event Details {">"}</button>
             </div>
         </div>
     )
