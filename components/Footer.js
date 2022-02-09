80f95a90a3b801f23a3745c2d4001b47d908e708
s3lven <erizjsub2@gmail.com>
2022-02-09 11:40:07 -0800

    started file structure

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+
+export default function Footer() {
+    return (
+        <Head>
+            
+        </Head>
+    )
+}
---
+++ pages/event.js
@@ -0,0 +1,13 @@
+export default function Event() {
+    return (
+        <>
+        {/* <Nav /> */}
+        <Head>
+            <title>Events | NVSI</title>
+        </Head>
+        {/* Grey Button Component
+        Event Component
+        <Footer /> */}
+        </>   
+    )
+}
