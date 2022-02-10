00d27fb1136b34272c66726f458dca90eb578e47
Derek Ma <danderekma@gmail.com>
2022-02-09 23:58:54 -0800

    Add welcome-background

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,18 +17,20 @@ const contact = [
 
 export default function Home() {
   return (
-    <div id={styles.homePage}>
-      <h1 id={styles.welcomeHeader}>WELCOME</h1>
-      <p id={styles.welcomeBody}>
-        Welcome to the New Viet Nam Studies Initiative where we bring together
-        scholars, industry experts, government officials, scientists, activists,
-        and artists to lend their knowledge of the many contemporary dimensions
-        of Viet Nam and its diasporic population. Highlighting the economic
-        reconstruction and cultural renewal through programing and publication
-        directly engages with the challenges and successes of this dynamic
-        nation.
-      </p>
-      <div id={styles.address}>
+    <div>
+      <div id={styles.welcomeSection}>
+        <h1 id={styles.welcomeHeader}>WELCOME</h1>
+        <p id={styles.welcomeBody}>
+          Welcome to the New Viet Nam Studies Initiative where we bring together
+          scholars, industry experts, government officials, scientists,
+          activists, and artists to lend their knowledge of the many
+          contemporary dimensions of Viet Nam and its diasporic population.
+          Highlighting the economic reconstruction and cultural renewal through
+          programing and publication directly engages with the challenges and
+          successes of this dynamic nation.
+        </p>
+      </div>
+      <div id={styles.addressSection}>
         <Address PhysAddress={address} ContactInfo={contact} />
       </div>
     </div>
