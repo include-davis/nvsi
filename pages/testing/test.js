631b4375264ba8a4ec0764466b3bcdaf4faafc9c
Khoi Nguyen <minhkhoi632000@gmail.com>
2022-02-11 18:42:48 -0800

    Add the EventPic component, use to create the circle picture element on the Event section on the Home Page
    Css might be a little bit scuffed. Still not responsive to different screen size yet

--- pages/testing/test.js
+++ pages/testing/test.js
@@ -1,111 +1,107 @@
-import Address from "../../components/address";
-import Circle from "../../components/addressCircle";
-
-
-// New Viet Nam Studies Initiative
-// 3113 Hart Hall
-// University of California Davis
-// One Shields Avenue
-// Davis, CA 95616
-//
-// Contact Us
-// newvietnamstudies@gmail.com
-// Phone: (530) 333 3137
-const address = [
-    "New Viet Nam Studies Initiative",
-    "3113 Hart Hall",
-    "University of California Davis",
-    "One Shields Avenue",
-    "Davis, CA 95616"
-]
-
-const contact = [
-    "Contact Us",
-    "newvietnamstudies@gmail.com",
-    "Phone: (530) 333 3137"
-]
-
-import styles from '../../libs/curvedText.module.css'
-
-function UpdateCurvedText ({ curvedText, radius }) {
-    curvedText.props.style.minWidth = "initial";
-    curvedText.props.style.minHeight = "initial";
-
-    let w = curvedText.props.style.width, h = curvedText.props.style.height;
-
-
-    curvedText.props.style.minWidth = w;
-    curvedText.props.style.minHeight = h;
-
-    w = w.slice(0, -2)
-    w = parseInt(w)
-    h = h.slice(0, -2)
-    h = parseInt(h)
-
-    let spans = [];
-    let text = curvedText.props.children;
-
-
-    let circleLength = 2 * Math.PI * radius;
-
-    let angleRad = w / (2 * radius);
-
-    let angle = 2 * angleRad * 180 / Math.PI / text.length;
-
-    Array.from(text).forEach((letter, index) => {
-
-        spans.push(<span key={index} className={styles.spanItem} style={{
-            position: "absolute",
-            height: `${radius}px`,
-            transformOrigin: "bottom center",
-        }}>
-            {letter}
-        </span>);
-    });
-
-    Array.from(spans).forEach(( elem, index ) => {
-        elem.props.style.transform = `translate(${w / 2}px,0px) rotate(${index * angle - text.length * angle / 2}deg)`
-        console.log(elem.props.style)
-    })
-
-    return (
-        <div className={styles.text}>
-            {spans}
-        </div>
-    )
-}
-
+import EventPic from "../../components/EventPic";
 
 export default function Test () {
-    // const text = "New Viet Nam Studies Initiative";
-    // const [textStyle, changeTextSyle] = useMeasure(text);
-    console.log("Here")
-    console.log(styles.text)
-    const text = <p
-        style={{
-            minWidth: '0.5em',
-            minHeight: '0.5em',
-            width: '500px',
-            height: '500px',
-            
-            position: 'relative',
-            display: 'inline-block',
-            margin: '0 auto',
-            fontSize: '100px'
-        }}
-        >Wow this is awesome
-    </p>;
-    console.log(text);
     return (
-        <>
-            <UpdateCurvedText curvedText={text} radius={1000} />
-            <Circle Text={"New Viet Nam Studies Initiative"} Type="left" />
-            {/* <Address PhysAddress={address} ContactInfo={contact} /> */}
-            {/* <p className={styles.rotateLeft}>{PhysAddress[0]}</p>
-            <p className={styles.rotateRight}>{PhysAddress[0]}</p> */}
-        </>
-    );
+        <EventPic nextLink="https://www.google.com" imgSrc={'/images/welcome-background.svg'}/>
+    )
 }
-
-
-
+// import Address from "../../components/address";
+// import Circle from "../../components/addressCircle";
+// 
+// import { useRef } from "react/cjs/react.production.min";
+// 
+// // New Viet Nam Studies Initiative
+// // 3113 Hart Hall
+// // University of California Davis
+// // One Shields Avenue
+// // Davis, CA 95616
+// //
+// // Contact Us
+// // newvietnamstudies@gmail.com
+// // Phone: (530) 333 3137
+// const address = [
+//     "New Viet Nam Studies Initiative",
+//     "3113 Hart Hall",
+//     "University of California Davis",
+//     "One Shields Avenue",
+//     "Davis, CA 95616"
+// ]
+// 
+// const contact = [
+//     "Contact Us",
+//     "newvietnamstudies@gmail.com",
+//     "Phone: (530) 333 3137"
+// ]
+// 
+// import styles from '../../libs/curvedText.module.css'
+// 
+// function UpdateCurvedText ({curvedText, radius, children}) {
+// 
+//     console.log(children)
+// 
+//     // let w = curvedText.props.style.width, h = curvedText.props.style.height;
+//     // let w = , h = styles.text.height;
+//     // console.log(html);
+// // 
+// //     curvedText.props.style.minWidth = w;
+// //     curvedText.props.style.minHeight = h;
+// // 
+// //     w = w.slice(0, -2)
+// //     w = parseInt(w)
+// //     h = h.slice(0, -2)
+// //     h = parseInt(h)
+// // 
+// //     let spans = [];
+// //     let text = curvedText.props.children;
+// // 
+// // 
+// //     let circleLength = 2 * Math.PI * radius;
+// // 
+// //     let angleRad = w / (2 * radius);
+// // 
+// //     let angle = 2 * angleRad * 180 / Math.PI / text.length;
+// // 
+// //     Array.from(text).forEach((letter, index) => {
+// // 
+// //         spans.push(<span key={index} className={styles.spanItem} style={{
+// //             position: "absolute",
+// //             height: `${radius}px`,
+// //             transformOrigin: "bottom center",
+// //         }}>
+// //             {letter}
+// //         </span>);
+// //     });
+// // 
+// //     Array.from(spans).forEach(( elem, index ) => {
+// //         elem.props.style.transform = `translate(${w / 2}px,0px) rotate(${index * angle - text.length * angle / 2}deg)`
+// //         console.log(elem.props.style)
+// //     })
+// 
+//     return (
+//         <div className={styles.text}>
+//             {}
+//         </div>
+//     )
+// }
+// 
+// 
+// export default function Test () {
+//     const text = <div className={styles.text}>"New Viet Nam Studies Initiative"</div>
+//     // const [textStyle, changeTextSyle] = useMeasure(text);
+//     // console.log("Here")
+//     // console.log(styles.text)
+// 
+//     // console.log(text);
+//     return (
+//         <>
+//             <UpdateCurvedText curvedText="This is cool" radius={1000} >{text}</UpdateCurvedText>
+//             <Circle Text={"New Viet Nam Studies Initiative"} Type="left" />
+//             {/* <Address PhysAddress={address} ContactInfo={contact} /> */}
+//             {/* <p className={styles.rotateLeft}>{PhysAddress[0]}</p>
+//             <p className={styles.rotateRight}>{PhysAddress[0]}</p> */}
+//         </>
+//     );
+// }
+// 
+// 
+// 
