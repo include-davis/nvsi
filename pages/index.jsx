3243399f1e849b321a8ba9c20c7fc9e9a37178c8
Derek Ma <danderekma@gmail.com>
2022-02-11 18:46:37 -0800

    Change spinningLogo and arrowIcon styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,22 +19,18 @@ export default function Home() {
   return (
     <div>
       <div id={styles.welcomeSection}>
-        <div>
-          <h1 id={styles.welcomeHeader}>WELCOME</h1>
-          <p id={styles.welcomeBody}>
-            Welcome to the New Viet Nam Studies Initiative where we bring
-            together scholars, industry experts, government officials,
-            scientists, activists, and artists to lend their knowledge of the
-            many contemporary dimensions of Viet Nam and its diasporic
-            population. Highlighting the economic reconstruction and cultural
-            renewal through programing and publication directly engages with the
-            challenges and successes of this dynamic nation.
-          </p>
-        </div>
-        <div id={styles.spinningSection}>
-          <img id={styles.spinningLogo} src={"/images/spinning-logo.svg"} />
-          <img id={styles.arrowIcon} src={"/images/arrow-icon.svg"} />
-        </div>
+        <h1 id={styles.welcomeHeader}>WELCOME</h1>
+        <p id={styles.welcomeBody}>
+          Welcome to the New Viet Nam Studies Initiative where we bring together
+          scholars, industry experts, government officials, scientists,
+          activists, and artists to lend their knowledge of the many
+          contemporary dimensions of Viet Nam and its diasporic population.
+          Highlighting the economic reconstruction and cultural renewal through
+          programing and publication directly engages with the challenges and
+          successes of this dynamic nation.
+        </p>
+        <img id={styles.spinningLogo} src={"/images/spinning-logo.svg"} />
+        <img id={styles.arrowIcon} src={"/images/arrow-icon.svg"} />
       </div>
       <div id={styles.addressSection}>
         <Address PhysAddress={address} ContactInfo={contact} />
@@ -44,13 +40,16 @@ export default function Home() {
       <div id={styles.formSection}>
         <div id={styles.formBody}>
           <h2 id={styles.formHeader}>KEEP POSTED WITH NVSI</h2>
-          <p id={styles.formText}>Update on events | How to get involved | Contact us directly</p>
+          <p id={styles.formText}>
+            Update on events | How to get involved | Contact us directly
+          </p>
           <div id={styles.formIcons}>
-            <img id={styles.facebookIcon} src={"/images/facebook-icon.svg"}/>
-            <img id={styles.youtubeIcon} src={"/images/youtube-icon.svg"}/>
-            <img id={styles.instagramIcon} src={"/images/instagram-icon.svg"}/>
+            <img id={styles.facebookIcon} src={"/images/facebook-icon.svg"} />
+            <img id={styles.youtubeIcon} src={"/images/youtube-icon.svg"} />
+            <img id={styles.instagramIcon} src={"/images/instagram-icon.svg"} />
           </div>
         </div>
+        <div id={styles.formInput}></div>
       </div>
     </div>
   );
