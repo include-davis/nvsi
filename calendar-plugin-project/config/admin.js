bb8c3216761d8aa4f2f8080ecab37924b1efbcaa
Austin Shih <austinhshih@gmail.com>
2022-02-19 15:44:49 -0800

    Created Event Type

--- calendar-plugin-project/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '8ef2d51070802c9687a97a47ad45183e'),
-  },
-});
--- calendar-plugin-project/src/plugins/calendarPlugin/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin calendarPlugin
-
-A quick description of calendarPlugin.
