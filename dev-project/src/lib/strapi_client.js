0099d91bd975deffabba1aec7da6e8baf4d1227a
Austin Shih <austinhshih@gmail.com>
2022-02-20 18:20:22 -0800

    Sorts Events By Time

--- dev-project/src/lib/strapi_client.js
+++ dev-project/src/lib/strapi_client.js
@@ -1,13 +1,40 @@
-request = require('request');
-
 class StrapiClient {
     constructor() {}
 
     async fetchData(path) {
-        const requestURL = `${process.env.STRAPI_API_URL}${path}`;
-        const response = await request(requestURL);
-        const data = await response.json();
-        return data;
+        const events = await strapi.api.event.services.event.find({});
+        var eventsList = events.results;
+        this.sortEventsByTime(eventsList);
+        console.log(eventsList);
+        return events;
+    }
+
+    dateToNumber(date) {
+        return parseInt(date.toString().replaceAll("-", ""));
+    }
+
+    timeToNumber(time) {
+        return parseInt(time.toString().replaceAll(":", "").replaceAll(".", ""));
+    }
+
+    sortEventsByTime(eventsList) {
+        eventsList.sort((event1, event2) => {
+            var output = 0;
+            const dateCompare = (this.dateToNumber(event1.Date) - this.dateToNumber(event2.Date));
+            if (dateCompare > 0) {
+                output = 1;
+            } else if (dateCompare < 0) {
+                output = -1;
+            } else {
+                const timeCompare = (this.timeToNumber(event1.StartTime) - this.timeToNumber(event2.StartTime));
+                if (timeCompare > 0) {
+                    output = 1;
+                } else if (timeCompare < 0) {
+                    output = -1;
+                }
+            }
+            return output;
+        });
     }
 }
 
