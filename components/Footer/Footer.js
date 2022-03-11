e4a3e5a4921191d9132208a749724a8e5dc2f19c
s3lven <erizjsub2@gmail.com>
2022-03-10 21:39:03 -0800

    adjusted footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,18 +4,17 @@ import Image from 'next/image'
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <div className={styles.grid}>
-                <div className={styles.left}>
-                    <h1>KEEP POSTED WITH NVSI</h1>
-                    <p>Update on events | How to get involved | Contact us directly</p>
+            <div className={styles.box}>
+                <div className={styles.grid}>
                     <div className={styles.socialmedia}>
                         <Image src="/icons/icon-facebook-dark_grey.svg" alt="Facebook Icon" width='24px' height='24px'/>
                         <Image src="/icons/icon-youtube-darkgrey.svg" alt="Youtube Icon" width='24px' height='24px'/>                    
-                    </div>
-                </div> 
-                <button>Subscribe</button>
+                    </div> 
+                    <h1>KEEP POSTED WITH NVSI</h1>
+                    <p>Update on events | How to get involved | Contact us directly</p>
+                    <button>Subscribe</button>
+                </div>
             </div>
-            
         </div>
     )
 }
