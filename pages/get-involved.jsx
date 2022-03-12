db3201e11f2fc95d661cdb4c35905991db789c84
Derek Ma <danderekma@gmail.com>
2022-03-11 20:14:03 -0800

    Complete get-involved page

--- pages/get-involved.jsx
+++ pages/get-involved.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TestimonialCard from "../components/get-involved/TestimonialCard";
 import styles from "../styles/get-involved.module.css";
 
 const GetInvolved = () => {
@@ -11,9 +12,32 @@ const GetInvolved = () => {
       <div className={styles.mainSection}>
         <div className={styles.testimonialsSection}>
           <h2 style={{ fontSize: "36px" }}>TESTIMONIALS</h2>
+          <TestimonialCard
+            imgSrc={"/images/get-involved/placeholder-headshot.svg"}
+            text={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam."
+            }
+          />
+          <TestimonialCard
+            imgSrc={"/images/get-involved/placeholder-headshot.svg"}
+            text={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam."
+            }
+          />
         </div>
-        <div className={styles.descriptionSection}>Hello</div>
+        <div className={styles.descriptionSection}>
+          <h2 className={styles.descriptionHeader}>DESCRIPTION</h2>
+          <p className={styles.descriptionText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam. Suspendisse at
+            convallis nibh, sed fermentum urna. Maecenas congue viverra ornare. Proin convallis lorem imperdiet enim
+            egestas mollis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium nec urna in aliquam.
+            Suspendisse at convallis nibh, sed fermentum urna. Maecenas congue viverra ornare. Proin convallis lorem
+            imperdiet enim egestas mollis.
+          </p>
+          <button className={styles.descriptionBtn}>Apply Here</button>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
