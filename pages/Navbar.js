a9f47acca78832a5560befd6e809fe6e8e015de0
yugii28 <yugandhar28@hotmail.com>
2022-03-24 14:14:09 -0700

    header and navbar

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css' 
 
-function Navbar() {
-    return <div className = 'navbar'>
-        <img src = "https://lh3.googleusercontent.com/PgacuOOq5JLBwdFKtz5Mo4H9O5B6VFjDbrw-yYuXqqndQ55GVousoIjpIlFI9-GG-wycsI0Ojdx7S1dsh5If_y9mYXecyEWagyEuxV7H0ysvt5vzajHNdx_oSiCLaVUlUuVeciPM=w2400" alt = "box" width = "300"/>
-        <div className = 'navlink'>
-        <NavLink activeClassName =  'active' className = "link" to = "/about"> About</NavLink>
-        <NavLink activeClassName =  'active' className = "link" to = "/events">Events</NavLink>
-        <NavLink activeClassName =  'active' className = "link" to = "/community">Community</NavLink>
-        <NavLink activeClassName =  'active' className = "link" to = "/whitepapers">White Papers</NavLink>
-        <NavLink activeClassName =  'active' className = "link" to = "/involved">Get Involved</NavLink>
-        </div>
-        </div>
-        
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+    <img src = "https://lh3.googleusercontent.com/PgacuOOq5JLBwdFKtz5Mo4H9O5B6VFjDbrw-yYuXqqndQ55GVousoIjpIlFI9-GG-wycsI0Ojdx7S1dsh5If_y9mYXecyEWagyEuxV7H0ysvt5vzajHNdx_oSiCLaVUlUuVeciPM=w2400" alt = "box" width = "300"/>
+      <div className= {styles.navlink}>
+      <Link href="/"><a className= {styles.link}>Home</a></Link>
+      <Link href="/about"><a className= {styles.link} >About</a></Link>
+      <Link href="/events"><a className= {styles.link}>Events</a></Link>
+      <Link  href="/community"><a className= {styles.link}>Community</a></Link>
+      <Link href="/whitepapers"><a className= {styles.link} >White Papers</a></Link>
+      <Link href="/getinvolved"><a className= {styles.link} >Get Involved</a></Link>
+      </div>
+    </nav>
+);
 }
-
+ 
 export default Navbar;
