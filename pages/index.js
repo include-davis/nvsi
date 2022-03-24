d746a2d1fbb09e47ea296acc56423c2e3955b424
Khoi Nguyen <minhkhoi632000@gmail.com>
2022-03-23 20:26:18 -0700

    Add popup to the about Us page.
    Still need to ask the design team what kind of interaction with the pop up they want.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
-// import Head from 'next/head'
+import { useState } from 'react/cjs/react.development'
 import Image from 'next/image'
 import Link from 'next/link'
+
 import styles from '../styles/aboutPage.module.css'
+
 import PillarCard from '../components/about/pillarCard'
+import PopupInfo from '../components/about/popupWindow'
+
+
+const PopUp = (setPopUp, { Name, PicPath, colorHexCode, Email, Info }) => {
+  return (
+    <>
+      <div className={styles.colorBG} style={{ backgroundColor: colorHexCode }} onClick={() => setPopUp(false)} />
+      <div className={styles.modal}>
+        <PopupInfo Name={Name} PicPath={PicPath} colorHexCode={colorHexCode} Email={Email}
+          Info={Info}
+        />
+      </div>
+    </>
+  )
+}
+
+
+const person1Data = {
+  Name: "Kieu Linh Valverde",
+  PicPath: '/images/about/placeholder.png',
+  colorHexCode: '#345707',
+  Email: "cvalverde@ucdavis.edu",
+  Info: "Dr. Kieu-Linh Caroline Valverde is Associate Professor of Asian American Studies and Founding Director of the New Viet Nam Studies Initiative at the University of California, Davis. She was a Fulbright, Rockefeller, and Luce scholar. Her teaching, research, and organizing interests include: Higher Education, Southeast Asian American history and contemporary issues, Mixed Race and Gender theories, Social Movements, Fashionology, National Branding, Spirit Realm, Diaspora, and Transnationalism studies. She authored Transnationalizing Viet Nam: Community, Culture, and Politics in the Diaspora (Temple University Press 2012). She founded the movement Fight the Tower with women of color and in the academy and co-edited the anthology, Fight the Tower: Asian American Women Scholars’ Resistance and Renewal in the Academy (Rutgers University Press 2020). Professor Valverde is working on curating an exhibit on youth culture and development in contemporary Viet Nam through the lens of Viet Nam’s national dress, the ao dai. She is also working on her third manuscript, which will examine national (re)branding projects with a focus on Viet Nam’s development in the area of sustainability and sovereignty. Her future research project looks at spirit realm beliefs and the history of its exclusion from the US academy, as well as the importance of its return."
+}
 
 export default function Home () {
+  const [isPopUp, setPopUp] = useState(false);
+
   return (
     <div className={styles.pageContainer}>
       <div className={styles.headerSection}>
@@ -50,6 +78,13 @@ export default function Home () {
       <h2>Research Assistant | Senior studying Psychology @ UC Davis</h2>
       <div className="Angie">Angie Pham</div>
       <h4>Research Assistant | Junior studying Psychology, Asian American Studies, Physics @ UC Davis</h4>
+
+      {/* Put div that cause the pop up here */}
+      <div className="What ever Name you like" onMouseOver={() => setPopUp(true)}>
+        {/* <button>Move mouse accross this to see effects</button> */}
+        <p>Test paragraph, can be replaced with pictures or anything you like (or just remove). Move mouse over this text to see effects!</p>
+        {isPopUp && PopUp(setPopUp, person1Data)}
+      </div>
 
       <div className="contact_rectangle">
         <div className="keep">Keep posted  with NVSI</div>
