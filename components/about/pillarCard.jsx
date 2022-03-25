9ac2fe07bb9d8d206a80cfa2bc3fafd4e980b5f6
Hannah Aldor <hkaldor@ucdavis.edu>
2022-03-24 18:16:26 -0700

    Took out icons for now since I don't know how to size them.

--- components/about/pillarCard.jsx
+++ components/about/pillarCard.jsx
@@ -4,10 +4,11 @@ import styles from '../../styles/pillarCard.module.css'
 export default function PillarCard ({ CardTitle, IconPicPath }) {
     return (
         <div className={styles.cardContainer}>
-            <div className={styles.cardContents}>
-                {CardTitle}
+            <div className = {styles.cardTitle}>{CardTitle}</div>
+            {/* <div>
                 <Image className = {styles.cardIcon} src={IconPicPath} layout='fill'></Image>
-            </div>  
+            </div> */}
         </div>
+
     )
 }
