4413633b426fd260ffe4bcb5bf92e4c35a998696
christunaroll <christina.vo2001@gmail.com>
2022-04-03 00:23:01 -0700

    initial responsiveness commit

--- pages/index.js
+++ pages/index.js
@@ -11,47 +11,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            <p>VIET NAM AND THE DIASPORA SILICON VALLEY CONNECTIONS</p>
+          </div>
         </div>
-      </main>
+    </div>
 
+    <div className={styles.circle}>
+      <div className={styles.flyer}>Flyer</div>
+    </div>
+
+
+    <div className={styles.main2}>
+      <div className={styles.paper_side}>
+        <div>DOCUMENT GOES HERE</div>
+      </div>
+
+          <div className={styles.tabs_container}>
+              <div className={styles.diversity}>Diversity</div>
+              <div className={styles.environment}>Environment</div>
+              <div className={styles.APPI_tab}>APPI</div>
+          </div>
+
+          <div className={styles.authors_container}>
+              <div className={styles.authors_title}>Authors</div>
+              <div className={styles.authors_names}>
+                <li className={styles.authors_list}>Name</li>
+                <li className={styles.authors_list}>Name</li>
+              </div>
+
+       </div>
+
+    </div>
+
+
+
+      {/* FOOTER DONT TOUCH */}
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
