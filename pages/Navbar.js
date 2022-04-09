837d7e67358eed72a5920a6d818dc11fe6f9cf56
yugii28 <yugandhar28@hotmail.com>
2022-04-08 17:42:33 -0700

    updated navbar
    updated navbar

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -4,12 +4,14 @@ import styles from '../styles/Home.module.css'
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-    <img src = "https://lh3.googleusercontent.com/PgacuOOq5JLBwdFKtz5Mo4H9O5B6VFjDbrw-yYuXqqndQ55GVousoIjpIlFI9-GG-wycsI0Ojdx7S1dsh5If_y9mYXecyEWagyEuxV7H0ysvt5vzajHNdx_oSiCLaVUlUuVeciPM=w2400" alt = "box" width = "300"/>
+      <div className={styles.image}>
+        <img src = "https://lh3.googleusercontent.com/PgacuOOq5JLBwdFKtz5Mo4H9O5B6VFjDbrw-yYuXqqndQ55GVousoIjpIlFI9-GG-wycsI0Ojdx7S1dsh5If_y9mYXecyEWagyEuxV7H0ysvt5vzajHNdx_oSiCLaVUlUuVeciPM=w2400" alt = "box" width = "300"/>
+      </div>
       <div className= {styles.navlink}>
       <Link href="/"><a className= {styles.link}>Home</a></Link>
-      <Link href="/about"><a className= {styles.link} >About</a></Link>
+      <Link href="/about"><a className= {styles.underline} >About</a></Link>
       <Link href="/events"><a className= {styles.link}>Events</a></Link>
-      <Link  href="/community"><a className= {styles.link}>Community</a></Link>
+      <Link href="/community"><a className= {styles.link}>Community</a></Link>
       <Link href="/whitepapers"><a className= {styles.link} >White Papers</a></Link>
       <Link href="/getinvolved"><a className= {styles.link} >Get Involved</a></Link>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className = {styles.background}>
+    <div>
+      <img src = "/vercel.svg"/>
+    <div>
       <div className={styles.height}>
-      <Navbar />
+        <Navbar />
       </div>
-      <h1 className = {styles.Events}>EVENTS</h1>
+      <div className = {styles.Events}>
+        <h1>EVENTS</h1>
+      </div>
+    </div>
     </div>
   )
 }
