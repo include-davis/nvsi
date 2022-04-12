449f132616e9c63ed445c89e58ca9fa23854f982
Aarush Agrawal <aarush.agrawal@gmail.com>
2022-04-11 18:32:27 -0700

    removed unnecessary content types

--- dev-project/src/api/keyword/controllers/keyword.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  keyword controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::keyword.keyword');
--- dev-project/src/api/keyword/routes/keyword.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * keyword router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::keyword.keyword');
