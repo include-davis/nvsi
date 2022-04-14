787c22d767391cedfde3255ad88759408f2f1de7
Hannah Aldor <hkaldor@ucdavis.edu>
2022-04-13 22:26:40 -0700

    Added pillar icons

--- components/about/pillarCard.jsx
+++ components/about/pillarCard.jsx
@@ -5,10 +5,10 @@ export default function PillarCard ({ CardTitle, IconPicPath }) {
     return (
         <div className={styles.cardContainer}>
             <div className = {styles.cardTitle}>{CardTitle}</div>
-            <div className = {styles.cardIcon}></div>
-            {/* <div>
-                <Image className = {styles.cardIcon} src={IconPicPath} layout='fill'></Image>
-            </div> */}
+            {/* <div className = {styles.cardIcon}></div> */}
+            <div className = {styles.cardIcon}>
+                <Image  src={IconPicPath} layout='fill'></Image>
+            </div>
         </div>
 
     )
