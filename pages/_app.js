d93a498f327ab3122431de776f8fc1837d13e3d5
Alec Atienza <alec@null.net>
2022-04-14 18:24:39 -0700

    merge conflicts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import "../styles/globals.css";
-=======
-import '../styles/globals.css'
-import '../styles/Home.module.css'
-
->>>>>>> main
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
