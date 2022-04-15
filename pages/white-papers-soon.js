ab2dba3b402f2d8b1a6ccca8e5adcdd82bbdda32
Alec Atienza <alec@null.net>
2022-04-14 19:43:54 -0700

    adjustments before merge

--- pages/white-papers-soon.js
+++ pages/white-papers-soon.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/whitepapers/WhitePapersSoon.module.css'
 
 export default function Home() {
   return (
@@ -31,21 +31,6 @@ export default function Home() {
     {/*TASKS:
       - fix font
       */}
-
-
-      {/* FOOTER DONT TOUCH */}
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
