c3594536c517d70be8bd1125e1f9fbb819c60e41
Alec Atienza <alec@null.net>
2022-04-29 13:45:54 -0700

    whitepaper page done

--- pages/white-papers/[id].js
+++ pages/white-papers/[id].js
@@ -16,7 +16,7 @@ import styles from "../../styles/whitepapers/WhitePapersDoc.module.css"
 //   ssr: false
 // })
 
-export default function WhitePaper({ link, title, author, topic }) {
+export default function WhitePaper({ size, link, title, author, tags }) {
   const [pageNumber, setPageNumber] = useState(1)
   const [numPages, setNumPages] = useState(null)
 
@@ -35,26 +35,48 @@ export default function WhitePaper({ link, title, author, topic }) {
       </div>
       <section className={styles.container}>
         <div className={styles.file}>
-          <a href={link}>Download {title}</a>
+          <a href={link}>
+            Download {title} ({size}MB)
+          </a>
           <div className={styles.wrapper}>
-            <Document file={link} onLoadSuccess={onLoad}>
+            <Document
+              className={styles.document}
+              file={link}
+              onLoadSuccess={onLoad}
+              // renderMode="svg"
+            >
               <Page className={styles.page} pageNumber={pageNumber} />
             </Document>
-            <div>
+            <div className={styles.buttons}>
               <button
+                disabled={pageNumber === 1}
                 onClick={() => setPageNumber((num) => Math.max(1, num - 1))}
               >
-                {"<"}
+                {"< Prev"}
               </button>
               <button
+                disabled={pageNumber === numPages}
                 onClick={() =>
                   setPageNumber((num) => Math.min(numPages, num + 1))
                 }
               >
-                {">"}
+                {"Next >"}
               </button>
             </div>
           </div>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.tags}>
+            {tags.map((tag) => (
+              <p key={tag}>{tag}</p>
+            ))}
+          </div>
+          <h2>Author(s)</h2>
+          <ul className={styles.authors}>
+            {author.map((auth) => (
+              <li key={auth}>{auth}</li>
+            ))}
+          </ul>
         </div>
       </section>
     </>
@@ -83,13 +105,15 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const paper = await getWhitePaperById(params.id)
+  // console.log(paper.attributes.Tags.split(";").slice(0, 3))
 
   return {
     props: {
       link: paper.attributes.File.data.attributes.url,
+      size: Math.round(paper.attributes.File.data.attributes.size) / 100,
       title: paper.attributes.Title,
-      author: paper.attributes.Author,
-      topic: paper.attributes.Topic,
+      author: paper.attributes.Author.split(";"),
+      tags: paper.attributes.Tags.split(";").slice(0, 3),
     },
   }
 }
